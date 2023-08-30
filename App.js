60202404ef6b51dbd1df89b83328c11ded9e9960
Mayank saini <mayanksaini4455@gmail.com>
2023-08-30 09:49:21 +0530

    Initial Commit with tailwind css configure

--- App.js
+++ App.js
@@ -3,8 +3,8 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.container} >
+      <Text className="text-red-900 text-xl">Open up to Yes start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
